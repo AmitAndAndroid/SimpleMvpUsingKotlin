044475931863ada8d56ec3c8f2082a3a37f93aab
AmitAndAAndroid <mullaamit6@gmail.com>
2020-10-10 13:06:11 +0530

    Simple Mvp Demo

---
+++ app/src/main/java/fusion/aim/simplemvpusingkotlin/views/activities/DummyActivity.kt
@@ -0,0 +1,13 @@
+package fusion.aim.simplemvpusingkotlin.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fusion.aim.simplemvpusingkotlin.R
+
+class DummyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dummy)
+    }
+}
